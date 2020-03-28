1ea9f871f24b56f5e9a1570ea77a52c31bd29d00
vanthuan0306 <vanthuan0306@users.noreply.github.com>
2020-03-28 08:17:02 +0700

    chương 3

---
+++ Chương 3/GiaNgauNhien/GiaNgauNhien/frmGiaNgauNhien.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace GiaNgauNhien
+{
+    partial class frmGiaNgauNhien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.gbHatGiong = new System.Windows.Forms.GroupBox();
+            this.lblLan = new System.Windows.Forms.Label();
+            this.nudSoLan = new System.Windows.Forms.NumericUpDown();
+            this.nudHatGiongDen = new System.Windows.Forms.NumericUpDown();
+            this.nudHatGiongTu = new System.Windows.Forms.NumericUpDown();
+            this.lblDen = new System.Windows.Forms.Label();
+            this.lblTu = new System.Windows.Forms.Label();
+            this.rbThoiGian = new System.Windows.Forms.RadioButton();
+            this.rbChiDinh = new System.Windows.Forms.RadioButton();
+            this.dgBangSoLan = new System.Windows.Forms.DataGridView();
+            this.dgcHatGiong = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgcSoLan = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnTaoNgauNhien = new System.Windows.Forms.Button();
+            this.btnDong = new System.Windows.Forms.Button();
+            this.stsStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.stsTienDo = new System.Windows.Forms.ToolStripProgressBar();
+            this.gbHatGiong.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoLan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHatGiongDen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHatGiongTu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBangSoLan)).BeginInit();
+            this.stsStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbHatGiong
+            // 
+            this.gbHatGiong.Controls.Add(this.lblLan);
+            this.gbHatGiong.Controls.Add(this.nudSoLan);
+            this.gbHatGiong.Controls.Add(this.nudHatGiongDen);
+            this.gbHatGiong.Controls.Add(this.nudHatGiongTu);
+            this.gbHatGiong.Controls.Add(this.lblDen);
+            this.gbHatGiong.Controls.Add(this.lblTu);
+            this.gbHatGiong.Controls.Add(this.rbThoiGian);
+            this.gbHatGiong.Controls.Add(this.rbChiDinh);
+            this.gbHatGiong.Location = new System.Drawing.Point(12, 12);
+            this.gbHatGiong.Name = "gbHatGiong";
+            this.gbHatGiong.Size = new System.Drawing.Size(374, 97);
+            this.gbHatGiong.TabIndex = 0;
+            this.gbHatGiong.TabStop = false;
+            this.gbHatGiong.Text = "Hạt giống";
+            // 
+            // lblLan
+            // 
+            this.lblLan.AutoSize = true;
+            this.lblLan.Location = new System.Drawing.Point(291, 65);
+            this.lblLan.Name = "lblLan";
+            this.lblLan.Size = new System.Drawing.Size(21, 13);
+            this.lblLan.TabIndex = 8;
+            this.lblLan.Text = "lần";
+            // 
+            // nudSoLan
+            // 
+            this.nudSoLan.Enabled = false;
+            this.nudSoLan.Location = new System.Drawing.Point(164, 59);
+            this.nudSoLan.Name = "nudSoLan";
+            this.nudSoLan.Size = new System.Drawing.Size(120, 20);
+            this.nudSoLan.TabIndex = 7;
+            this.nudSoLan.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudSoLan.ValueChanged += new System.EventHandler(this.nud_ValueChanged);
+            // 
+            // nudHatGiongDen
+            // 
+            this.nudHatGiongDen.Location = new System.Drawing.Point(258, 33);
+            this.nudHatGiongDen.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nudHatGiongDen.Name = "nudHatGiongDen";
+            this.nudHatGiongDen.Size = new System.Drawing.Size(90, 20);
+            this.nudHatGiongDen.TabIndex = 6;
+            this.nudHatGiongDen.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudHatGiongDen.ValueChanged += new System.EventHandler(this.nud_ValueChanged);
+            // 
+            // nudHatGiongTu
+            // 
+            this.nudHatGiongTu.Location = new System.Drawing.Point(164, 33);
+            this.nudHatGiongTu.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.nudHatGiongTu.Name = "nudHatGiongTu";
+            this.nudHatGiongTu.Size = new System.Drawing.Size(88, 20);
+            this.nudHatGiongTu.TabIndex = 5;
+            this.nudHatGiongTu.ValueChanged += new System.EventHandler(this.nud_ValueChanged);
+            // 
+            // lblDen
+            // 
+            this.lblDen.AutoSize = true;
+            this.lblDen.Location = new System.Drawing.Point(257, 16);
+            this.lblDen.Name = "lblDen";
+            this.lblDen.Size = new System.Drawing.Size(27, 13);
+            this.lblDen.TabIndex = 3;
+            this.lblDen.Text = "Đến";
+            // 
+            // lblTu
+            // 
+            this.lblTu.AutoSize = true;
+            this.lblTu.Location = new System.Drawing.Point(161, 16);
+            this.lblTu.Name = "lblTu";
+            this.lblTu.Size = new System.Drawing.Size(20, 13);
+            this.lblTu.TabIndex = 2;
+            this.lblTu.Text = "Từ";
+            // 
+            // rbThoiGian
+            // 
+            this.rbThoiGian.AutoSize = true;
+            this.rbThoiGian.Location = new System.Drawing.Point(6, 59);
+            this.rbThoiGian.Name = "rbThoiGian";
+            this.rbThoiGian.Size = new System.Drawing.Size(93, 17);
+            this.rbThoiGian.TabIndex = 1;
+            this.rbThoiGian.Text = "Theo thời gian";
+            this.rbThoiGian.UseVisualStyleBackColor = true;
+            this.rbThoiGian.CheckedChanged += new System.EventHandler(this.rbThoiGian_CheckedChanged);
+            // 
+            // rbChiDinh
+            // 
+            this.rbChiDinh.AutoSize = true;
+            this.rbChiDinh.Checked = true;
+            this.rbChiDinh.Location = new System.Drawing.Point(7, 37);
+            this.rbChiDinh.Name = "rbChiDinh";
+            this.rbChiDinh.Size = new System.Drawing.Size(64, 17);
+            this.rbChiDinh.TabIndex = 0;
+            this.rbChiDinh.TabStop = true;
+            this.rbChiDinh.Text = "Chỉ định";
+            this.rbChiDinh.UseVisualStyleBackColor = true;
+            this.rbChiDinh.CheckedChanged += new System.EventHandler(this.rbChiDinh_CheckedChanged);
+            // 
+            // dgBangSoLan
+            // 
+            this.dgBangSoLan.AllowUserToAddRows = false;
+            this.dgBangSoLan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBangSoLan.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgcHatGiong,
+            this.dgcSoLan});
+            this.dgBangSoLan.Location = new System.Drawing.Point(12, 115);
+            this.dgBangSoLan.Name = "dgBangSoLan";
+            this.dgBangSoLan.Size = new System.Drawing.Size(374, 189);
+            this.dgBangSoLan.TabIndex = 1;
+            // 
+            // dgcHatGiong
+            // 
+            this.dgcHatGiong.DataPropertyName = "HatGiong";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            this.dgcHatGiong.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgcHatGiong.HeaderText = "Hạt giống";
+            this.dgcHatGiong.Name = "dgcHatGiong";
+            // 
+            // dgcSoLan
+            // 
+            this.dgcSoLan.DataPropertyName = "SoLan";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.Format = "N0";
+            this.dgcSoLan.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgcSoLan.HeaderText = "Số lần";
+            this.dgcSoLan.Name = "dgcSoLan";
+            // 
+            // btnTaoNgauNhien
+            // 
+            this.btnTaoNgauNhien.Location = new System.Drawing.Point(40, 310);
+            this.btnTaoNgauNhien.Name = "btnTaoNgauNhien";
+            this.btnTaoNgauNhien.Size = new System.Drawing.Size(104, 23);
+            this.btnTaoNgauNhien.TabIndex = 2;
+            this.btnTaoNgauNhien.Text = "Tạo ngẫu nhiên";
+            this.btnTaoNgauNhien.UseVisualStyleBackColor = true;
+            this.btnTaoNgauNhien.Click += new System.EventHandler(this.btnTaoNgauNhien_Click);
+            // 
+            // btnDong
+            // 
+            this.btnDong.Location = new System.Drawing.Point(254, 310);
+            this.btnDong.Name = "btnDong";
+            this.btnDong.Size = new System.Drawing.Size(106, 23);
+            this.btnDong.TabIndex = 3;
+            this.btnDong.Text = "Đóng";
+            this.btnDong.UseVisualStyleBackColor = true;
+            this.btnDong.Click += new System.EventHandler(this.btnDong_Click);
+            // 
+            // stsStrip
+            // 
+            this.stsStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.stsTienDo});
+            this.stsStrip.Location = new System.Drawing.Point(0, 335);
+            this.stsStrip.Name = "stsStrip";
+            this.stsStrip.Size = new System.Drawing.Size(398, 22);
+            this.stsStrip.TabIndex = 4;
+            this.stsStrip.Text = "Trạng thái";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(100, 17);
+            this.toolStripStatusLabel1.Text = "Trạng thái";
+            // 
+            // stsTienDo
+            // 
+            this.stsTienDo.AutoSize = false;
+            this.stsTienDo.Name = "stsTienDo";
+            this.stsTienDo.Size = new System.Drawing.Size(280, 16);
+            // 
+            // frmGiaNgauNhien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 357);
+            this.Controls.Add(this.stsStrip);
+            this.Controls.Add(this.btnDong);
+            this.Controls.Add(this.btnTaoNgauNhien);
+            this.Controls.Add(this.dgBangSoLan);
+            this.Controls.Add(this.gbHatGiong);
+            this.Name = "frmGiaNgauNhien";
+            this.Text = "Bảng Giả Ngẫu nhiên";
+            this.Load += new System.EventHandler(this.frmGiaNgauNhien_Load);
+            this.gbHatGiong.ResumeLayout(false);
+            this.gbHatGiong.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSoLan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHatGiongDen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHatGiongTu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBangSoLan)).EndInit();
+            this.stsStrip.ResumeLayout(false);
+            this.stsStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbHatGiong;
+        private System.Windows.Forms.Label lblDen;
+        private System.Windows.Forms.Label lblTu;
+        private System.Windows.Forms.RadioButton rbThoiGian;
+        private System.Windows.Forms.RadioButton rbChiDinh;
+        private System.Windows.Forms.DataGridView dgBangSoLan;
+        private System.Windows.Forms.Button btnTaoNgauNhien;
+        private System.Windows.Forms.Button btnDong;
+        private System.Windows.Forms.StatusStrip stsStrip;
+        private System.Windows.Forms.NumericUpDown nudHatGiongDen;
+        private System.Windows.Forms.NumericUpDown nudHatGiongTu;
+        private System.Windows.Forms.Label lblLan;
+        private System.Windows.Forms.NumericUpDown nudSoLan;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar stsTienDo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcHatGiong;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgcSoLan;
+    }
+}
+
---
+++ Chương 3/GiaNgauNhien/GiaNgauNhien/frmGiaNgauNhien.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaNgauNhien
+{
+    public partial class frmGiaNgauNhien : Form
+    {
+        #region khai bao bien
+        private DataSet dsBangSoLan = new DataSet();
+        private DataTable dtBangSoLan = new DataTable();
+        #endregion
+        public frmGiaNgauNhien()
+        {
+            InitializeComponent();
+        }
+        #region casc phuong thuc
+        private long TimSoLan(int HatGiong)
+        {
+            Random RndObj = new Random(HatGiong);
+            int SoNgauNhienDauTien = RndObj.Next();
+            int SoNgauNhien;
+            long SoLan = 0;
+            do
+            {
+                SoNgauNhien = RndObj.Next();
+                SoLan++;
+            } while (SoNgauNhien != SoNgauNhienDauTien);
+            return SoLan;
+        }
+        private void ThemVaoBang(int HatGiong,long SoLan)
+        {
+            dtBangSoLan.Rows.Add(HatGiong, SoLan);
+        }
+        private void KiemHopLe()
+        {
+            btnTaoNgauNhien.Enabled = rbChiDinh.Checked && (nudHatGiongTu.Value <= nudHatGiongDen.Value)|| rbThoiGian.Checked && nudSoLan.Value > 0;
+        }
+        #endregion
+
+        #region cac xu ly
+        private void frmGiaNgauNhien_Load(object sender, EventArgs e)
+        {
+            dtBangSoLan.Columns.Add(new DataColumn("HatGiong", System.Type.GetType("System.Int32")));
+            dtBangSoLan.Columns.Add(new DataColumn("SoLan", System.Type.GetType("System.Decimal")));
+            dtBangSoLan.TableName = "BangSo";
+            dsBangSoLan.Tables.Add(dtBangSoLan);
+            dgBangSoLan.DataSource = dsBangSoLan.Tables["BangSo"]; // dtBangSoLan;
+        }
+
+        private void rbChiDinh_CheckedChanged(object sender, EventArgs e)
+        {
+            nudHatGiongTu.Enabled = true;
+            nudHatGiongDen.Enabled = true;
+            nudSoLan.Enabled = false;
+            KiemHopLe();
+        }
+
+        private void rbThoiGian_CheckedChanged(object sender, EventArgs e)
+        {
+            nudHatGiongTu.Enabled = false;
+            nudHatGiongDen.Enabled = false;
+            nudSoLan.Enabled = true;
+            KiemHopLe();
+        }
+
+        private void nud_ValueChanged(object sender, EventArgs e)
+        {
+            KiemHopLe();
+        }
+
+        private void btnTaoNgauNhien_Click(object sender, EventArgs e)
+        {
+            btnTaoNgauNhien.Enabled = false;
+            stsStrip.Items[0].Text = "Đang tính toán ...";
+            stsTienDo.Value = 0;
+            if (rbChiDinh.Checked)
+            {
+                int HatGiongTu = (int)nudHatGiongTu.Value;
+                int HatGiongDen = (int)nudHatGiongDen.Value;
+                dtBangSoLan.Rows.Clear();
+                Application.DoEvents();
+                stsTienDo.Step = 100 / (HatGiongDen - HatGiongTu + 1);
+                for (int HatGiong = HatGiongTu; HatGiong <= HatGiongDen; HatGiong++)
+                {
+                    stsTienDo.Value = (HatGiong - HatGiongTu + 1) * 100 / (HatGiongDen - HatGiongTu + 1);
+                    long SoLan = TimSoLan(HatGiong);
+                    ThemVaoBang(HatGiong, SoLan);
+                    Application.DoEvents();
+                }
+            }
+            else
+            {
+                dtBangSoLan.Rows.Clear();
+                Application.DoEvents();
+                int SoLanChay = (int)nudSoLan.Value;
+                stsTienDo.Step = 100 / SoLanChay;
+                for (int lan = 0; lan < (int)nudSoLan.Value; lan++)
+                {
+                    stsTienDo.Value = (lan + 1) * 100 / SoLanChay;
+                    int HatGiong = (int)DateTime.Now.Millisecond;
+                    long SoLan = TimSoLan(HatGiong);
+                    ThemVaoBang(HatGiong, SoLan);
+                    Application.DoEvents();
+                }
+            }
+            stsTienDo.Value = 100;
+            stsStrip.Items[0].Text = "Đã tính xong";
+            btnTaoNgauNhien.Enabled = true;
+        }
+
+        private void btnDong_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+    }
+}
